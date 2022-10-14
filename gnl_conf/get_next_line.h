bba4b629386a0bfc1f73862e809f8a8bba737f39
jbillet <jbillet@student.42sp.org.br>
2022-10-14 04:03:53 +0200

    Commit 2 13-10-2022

---
+++ gnl_conf/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbillet <jbillet@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/15 21:58:46 by jbillet           #+#    #+#             */
+/*   Updated: 2022/06/27 09:12:48 by jbillet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *stash, char *buf);
+
+#endif
